f568acf700e7f31f149d64c3548312e52db2c46b
Clément Creusat <clement.creusat@gmail.com>
2022-03-20 18:11:25 +0100

    interactive vue
    vue
    
    vue
    
    version html
    
    interactive rating react
    
    interactive rating react
    
    interactive rating vue
    
    interactive rating vue
    
    interactive rating vue
    
    interactive vue
    
    interactive vue
    
    interactive vue
    
    interactive vue

---
+++ src/components/Result.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import CardContainer from "@/components/CardContainer.vue";
+import IllustrationComponent from "@/components/IllustrationComponent.vue";
+import BlockText from "@/components/BlockText.vue";
+
+defineProps({
+  choice: Number,
+});
+</script>
+
+<template>
+  <CardContainer class="align-items-center">
+    <IllustrationComponent />
+
+    <span class="badge">
+      You selected <span>{{ choice }}</span> out of 5
+    </span>
+
+    <BlockText
+      title="Thank you"
+      text="We appreciate you taking the time to give a rating. If you ever need
+        more support, don’t hesitate to get in touch!"
+    />
+  </CardContainer>
+</template>
